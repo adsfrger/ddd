bf4b4f38f5d5232133690eeda6df10d9503a61db
adsfrger <taroneko486@gmail.com>
2025-04-19 10:26:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,42 +5,47 @@ const app = express();
 // HTMLフォームの表示
 app.get('/', (req, res) => {
   res.send(`
-    <form method="GET" action="/go">
-      <input name="url" placeholder="URLを入力してね" style="width:300px">
-      <button type="submit">表示</button>
-    </form>
-  `);
-});
-
-// プロキシ経由でページを取得
-app.get('/go', async (req, res) => {
-  const targetUrl = req.query.url;
-
-  if (!targetUrl) return res.send('URLを入力してください。');
-
-  let cleanedUrl = targetUrl;
-  if (!/^https?:\/\//i.test(cleanedUrl)) {
-    cleanedUrl = 'https://' + cleanedUrl;
-  }
-
-  try {
-    // そのURLからページを取得
-    const response = await axios.get(cleanedUrl);
-
-    // レスポンスのHTMLをそのまま返す
-    res.send(`
-      <html>
-        <head><title>${response.data.match(/<title>(.*?)<\/title>/)[1]}</title></head>
-        <body>
-          <div style="margin: 0; padding: 0; width: 100%; height: 100vh;">
-            ${response.data}
+    <html>
+      <head><title>Multi Tab Browser</title></head>
+      <body>
+        <h1>Multi Tab Browser</h1>
+        <div>
+          <ul id="tabs">
+            <li class="tab" data-tab="tab1">Google</li>
+            <li class="tab" data-tab="tab2">Yahoo</li>
+            <li class="tab" data-tab="tab3">GitHub</li>
+          </ul>
+          <div id="tab1" class="tab-content">
+            <iframe src="https://www.google.com" style="width:100%; height:80vh;"></iframe>
           </div>
-        </body>
-      </html>
-    `);
-  } catch (error) {
-    res.send('ページの取得に失敗しました。');
-  }
+          <div id="tab2" class="tab-content" style="display:none;">
+            <iframe src="https://www.yahoo.co.jp" style="width:100%; height:80vh;"></iframe>
+          </div>
+          <div id="tab3" class="tab-content" style="display:none;">
+            <iframe src="https://github.com" style="width:100%; height:80vh;"></iframe>
+          </div>
+        </div>
+      </body>
+      <script>
+        const tabs = document.querySelectorAll('.tab');
+        const tabContents = document.querySelectorAll('.tab-content');
+
+        tabs.forEach(tab => {
+          tab.addEventListener('click', () => {
+            const targetTab = tab.getAttribute('data-tab');
+            
+            tabContents.forEach(content => {
+              if(content.id === targetTab) {
+                content.style.display = 'block';
+              } else {
+                content.style.display = 'none';
+              }
+            });
+          });
+        });
+      </script>
+    </html>
+  `);
 });
 
 // サーバーを起動
