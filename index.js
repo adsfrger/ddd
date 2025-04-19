fca819acffdf1890dc4412ebe38429e4fe272fb2
adsfrger <taroneko486@gmail.com>
2025-04-19 10:29:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ app.get('/', (req, res) => {
             <li class="tab" data-tab="tab3">GitHub</li>
           </ul>
           <div id="tab1" class="tab-content">
-            <iframe src="https://www.google.com" style="width:100%; height:80vh;"></iframe>
+            <iframe src="/go?url=https://www.google.com" style="width:100%; height:80vh;"></iframe>
           </div>
           <div id="tab2" class="tab-content" style="display:none;">
-            <iframe src="https://www.yahoo.co.jp" style="width:100%; height:80vh;"></iframe>
+            <iframe src="/go?url=https://www.yahoo.co.jp" style="width:100%; height:80vh;"></iframe>
           </div>
           <div id="tab3" class="tab-content" style="display:none;">
-            <iframe src="https://github.com" style="width:100%; height:80vh;"></iframe>
+            <iframe src="/go?url=https://github.com" style="width:100%; height:80vh;"></iframe>
           </div>
         </div>
       </body>
@@ -46,6 +46,40 @@ app.get('/', (req, res) => {
       </script>
     </html>
   `);
+});
+
+// プロキシ経由でページを取得
+app.get('/go', async (req, res) => {
+  const targetUrl = req.query.url;
+
+  if (!targetUrl) return res.send('URLを入力してください。');
+
+  let cleanedUrl = targetUrl;
+  if (!/^https?:\/\//i.test(cleanedUrl)) {
+    cleanedUrl = 'https://' + cleanedUrl;
+  }
+
+  try {
+    // axiosでリダイレクトを無視する設定を追加
+    const response = await axios.get(cleanedUrl, {
+      maxRedirects: 0, // リダイレクトを追跡しない
+      validateStatus: (status) => status < 400, // 400番台はエラーと判定
+    });
+
+    // レスポンスのHTMLをそのまま返す
+    res.send(`
+      <html>
+        <head><title>${response.data.match(/<title>(.*?)<\/title>/)[1]}</title></head>
+        <body>
+          <div style="margin: 0; padding: 0; width: 100%; height: 100vh;">
+            ${response.data}
+          </div>
+        </body>
+      </html>
+    `);
+  } catch (error) {
+    res.send('ページの取得に失敗しました。');
+  }
 });
 
 // サーバーを起動
